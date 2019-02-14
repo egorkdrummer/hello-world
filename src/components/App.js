2a1a6b0ce6a0fc61a3f41faaeaf844cc0f3cf1f3
Melnik Igor <egorkdrummer@gmail.com>
2019-02-15 00:25:24 +0200

    Hello World

--- src/components/App.js
+++ src/components/App.js
@@ -4,7 +4,9 @@ import './App.css';
 class App extends Component {
 	render() {
 		return (
-			<div className="App"></div>
+			<div className="App">
+				<h4>Hello World</h4>
+			</div>
 		);
 	}
 }
